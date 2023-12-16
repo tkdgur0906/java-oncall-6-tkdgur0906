bee5e1b76f961f5cadd2332ec863954dd788d059
KimSangHyeok <tkdgur0906@naver.com>
2023-12-16 18:20:51 +0900

    test : 비상 근무표 계산 테스트 작성

--- src/test/java/oncall/controller/WorkControllerTest.java
+++ src/test/java/oncall/controller/WorkControllerTest.java
@@ -2,12 +2,15 @@ package oncall.controller;
 
 import oncall.domain.Date;
 import oncall.domain.Worker;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkControllerTest {
@@ -15,6 +18,7 @@ class WorkControllerTest {
     private WorkController workController = new WorkController();
 
     @Test
+    @DisplayName("비상 근무표 계산")
     void calculateWorkerOrder() {
         Date date = new Date(5,"월");
         List<String> a = Arrays.asList("준팍", "도밥", "고니", "수아", "루루", "글로", "솔로스타", "우코", "슬링키", "참새", "도리");
@@ -27,9 +31,8 @@ class WorkControllerTest {
                 .collect(Collectors.toList());
 
         List<Worker> workers = workController.calculateWorkerOrder(date, weekdayWorkers, holidayWorkers);
-        for (Worker worker : workers) {
-            System.out.println(worker.getName());
-        }
 
+        assertThat(workers.get(4).getName()).isEqualTo("루루");
+        assertThat(workers.get(5).getName()).isEqualTo("수아");
     }
 }
